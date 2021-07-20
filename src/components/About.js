c0484d9b8f2664b53b6793fc2e5fc625fc2b6a28
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-20 09:49:14 -0400

    Changed the class componenet for About.js into a function component

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import harviator from '../images/harviator.png';
 
-class About extends Component {
-
-    render() {
+function About(props) {
         return (
             <section id="about">
                 <img src={harviator} class="App-logo" alt="Jon Harvey Avatar" />
-                <h1>{this.props.ownerName}</h1>
+                <h1>{props.ownerName}</h1>
                 <p>Hi! I'm a pilot who is on a journey to become a developer</p>
             </section>
         );
-    }
 }
 
 export default About;
