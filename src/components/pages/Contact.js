c9b564689baabe081e800dbf93958b20f51c281f
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-22 19:45:31 -0400

    Finished updating and styling contact section

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,20 +7,22 @@ function Contact(props) {
         <Form className="form">
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Normal text" required />
+                <Form.Control type="text" placeholder="Put your name here mate" required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="name@example.com" required />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Message</Form.Label>
-                <Form.Control as="textarea" rows={3} required />
+                <Form.Control as="textarea" placeholder="Drop me a line mate" rows={3} required />
             </Form.Group>
             <div className="d-grid gap-2">
-                <Button variant="secondary" size="lg" type="submit">
+                <Button className="btn" variant="secondary" size="lg" type="submit">
                     Submit
                 </Button>
             </div>
-        </Form>
+        </Form >
     );
 }
 
