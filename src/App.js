7794c64d48330776a7d5b02f72f5b960ad6bf25e
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-23 12:18:03 -0400

    Finished adding content and styling the footer

--- src/App.js
+++ src/App.js
@@ -43,9 +43,7 @@ WHEN I am presented with the Portfolio section THEN I see titled images of six o
 WHEN I am presented with the Contact section THEN I see a contact form with fields for a name, an email address, and a message - Done
 WHEN I move my cursor out of one of the form fields without entering text THEN I receive a notification that this field is required - Done
 WHEN I enter text into the email address field THEN I receive a notification if I have entered an invalid email address - Done
-WHEN I am presented with the Resume section
-THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
-WHEN I view the footer
-THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter)
+WHEN I am presented with the Resume section THEN I see a link to a downloadable resume and a list of the developer’s proficiencies - Done
+WHEN I view the footer THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter) - Done
 
 */
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,27 @@
 import React from 'react';
+import liIcon from '../assets/images/LI-In-Bug.png';
+import soIcon from '../assets/images/stackoverflow-logo.png';
 
 function Footer(props) {
     return (
         <footer id="contact">
 
-            <h3>Let's build something great!</h3>
-
-            <section class="profile">
-
-                <a id="profile-link" href="mailto:harviator@gmail.com">E: harviator@gmail.com</a>
-
-                <a id="profile-link" href="tel:+1-416-555-0137">P: +1-416-555-0137</a>
-
-                
+            <section class="profile">               
 
                 <a id="profile-link" href="https://github.com/harviator" target="_blank">
-                    <img className="project-image"
+                    <img className="github-logo"
                         src="https://user-images.githubusercontent.com/78506767/113162427-4d0f2380-920d-11eb-88f9-bf0aec2c9029.png"
                         alt="GitHub Logo" />
                 </a>
 
                 <a id="profile-link" href="https://www.linkedin.com/" target="_blank">
-                    <img height="35" src="./assets/images/LI-In-Bug.png" alt="LinkedIn Logo" /></a>
+                    <img className="linkedin-logo" height="35" src={liIcon} alt="LinkedIn Logo" />
+                </a>
+
+                <a id="profile-link" href="https://stackoverflow.com/users/15560306/harviator?tab=profile" target="_blank">
+                    <img className="stackoverflow-logo" height="35" src={soIcon} alt="Stackoverflow Logo" />
+                </a>
+
             </section>
 
         </footer>
