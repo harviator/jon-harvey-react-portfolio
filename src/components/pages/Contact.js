6921a944ec15eda8e4bfc66494dd33d84603e9be
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-23 08:24:59 -0400

    Updated the contact and portfolio sections

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,25 +4,28 @@ import Button from 'react-bootstrap/Button'
 
 function Contact(props) {
     return (
-        <Form className="form">
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Put your name here mate" required />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" required />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Message</Form.Label>
-                <Form.Control as="textarea" placeholder="Drop me a line mate" rows={3} required />
-            </Form.Group>
-            <div className="d-grid gap-2">
-                <Button className="btn" variant="secondary" size="lg" type="submit">
-                    Submit
-                </Button>
-            </div>
-        </Form >
+        <section>
+            <h2>Contact Me</h2>
+            <Form className="form">
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Put your name here mate" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control as="textarea" placeholder="Drop me a line mate" rows={3} required />
+                </Form.Group>
+                <div className="d-grid gap-2">
+                    <Button className="btn" variant="secondary" size="lg" type="submit">
+                        Submit
+                    </Button>
+                </div>
+            </Form >
+        </section>
     );
 }
 
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,20 +2,26 @@ import React from 'react';
 
 function Resume(props) {
     return (
-        <div>
+        <section>
             <h2>Resume</h2>
+
+            <p>Download my <a id="profile-link"
+                href="https://drive.google.com/file/d/1a6SfhS_a1vxUoSp16MjsUUqtMsVMLzDk/view?usp=sharing"
+                target="_blank">Resume</a>
+            </p>
+
+            <p>Front-End Technologies</p>
+
 
             <p>Cool cool cool cool cool</p>
             <p>No doubt no doubt no doubt no doubt no doubt</p>
             <p>Cool cool cool cool cool</p>
             <p>No doubt no doubt no doubt no doubt no doubt</p>
-            <p>Cool cool cool cool cool</p>
+            <p>Back-End Technologies</p>
             <p>No doubt no doubt no doubt no doubt no doubt</p>
             <p>Cool cool cool cool cool</p>
             <p>No doubt no doubt no doubt no doubt no doubt</p>
-            <p>Cool cool cool cool cool</p>
-            <p>No doubt no doubt no doubt no doubt no doubt</p>
-        </div>
+        </section>
     );
 }
 
